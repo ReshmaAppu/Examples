c3672555da8544c872b9b386100a42b880754c33
ReshmaAppu <57393385+ReshmaAppu@users.noreply.github.com>
2019-11-05 15:13:58 +0530

    Update ubidots.py

--- python/ubidots.py
+++ python/ubidots.py
@@ -3,7 +3,7 @@ import requests
 import math
 import random
 
-TOKEN = "BBFF-hoQmnfGcH9KyIY86gsDCxuh3qpbbVT"  # Put your TOKEN here
+TOKEN = "BBFF-GtKlon3sta6cz7bL95wlc7P2ZIVA3J"  # Put your TOKEN here
 DEVICE_LABEL = "machine"  # Put your device label here 
 VARIABLE_LABEL_1 = "temperature"  # Put your first variable label here
 VARIABLE_LABEL_2 = "humidity"  # Put your second variable label here
